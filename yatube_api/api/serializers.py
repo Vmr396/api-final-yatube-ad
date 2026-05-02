5364b595becdcfaabbf0e441bbad671a3598c78b
Vmr396 <milanavalikhomatova@gmail.com>
2026-05-02 10:32:03 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from posts.models import Post, Group, Comment, Follow  # noqa
+from posts.models import Follow
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
