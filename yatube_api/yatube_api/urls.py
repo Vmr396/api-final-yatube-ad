2040d5796520beb37565dda6945199bee3c1f577
Vmr396 <milanavalikhomatova@gmail.com>
2026-05-02 09:48:11 +0300

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+    path('api/', include('api.urls')),  # ваши api-эндпоинты (посты, фолловеры и т.д.)
+    
+    # JWT-эндпоинты для аутентификации
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
