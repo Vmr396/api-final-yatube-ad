2285b680756507647be57bc9b0598f73344fd263
Vmr396 <milanavalikhomatova@gmail.com>
2026-05-02 09:39:36 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from api.views import FollowViewSet
+
+router = DefaultRouter()
+router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
