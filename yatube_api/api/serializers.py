9732159188323c547039304f9f5528b8a3c60b1a
Vmr396 <milanavalikhomatova@gmail.com>
2026-05-02 10:04:16 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from posts.models import Follow
+from posts.models import Follow, Post, Group, Comment
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = serializers.StringRelatedField(read_only=True)
     following = serializers.SlugRelatedField(
         slug_field='username',
         queryset=User.objects.all()
@@ -14,9 +15,15 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
         fields = ('user', 'following')
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на этого пользователя'
+            )
+        ]
 
     def validate_following(self, value):
-        request = self.context.get('request')
-        if request and request.user == value:
-            raise serializers.ValidationError('Нельзя подписаться на самого себя.')
+        if self.context['request'].user == value:
+            raise serializers.ValidationError('Нельзя подписаться на самого себя')
         return value
