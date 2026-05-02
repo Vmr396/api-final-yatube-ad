3dcf18f9aca10a59f87640e12dd6871325f6b0a0
Vmr396 <milanavalikhomatova@gmail.com>
2026-05-02 10:26:45 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -9,7 +9,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10, 
+    'PAGE_SIZE': 10,
 }
 
 SIMPLE_JWT = {
